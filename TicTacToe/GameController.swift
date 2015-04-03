e59576e08ff710519b57de836b5ce59b7c0558a1
YiChenshu <ycswaves@gmail.com>
2015-04-03 14:07:57 +0800

    add play logic

--- TicTacToe/GameController.swift
+++ TicTacToe/GameController.swift
@@ -10,11 +10,53 @@ import Foundation
 import UIkit
 
 class GameController {
-    var isCross = true
-    func placeSymbol() -> UIImage {
+    
+    private var isCross = true
+    private var boardStatus = [0,0,0,0,0,0,0,0,0]
+    private var pieceCount = 0
+    private var winningStatuses = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    
+    func getSymbol() -> UIImage {
         var fileName = isCross ? "cross.png" : "nout.png"
         var image = UIImage(named: fileName)!
-        isCross = !isCross
         return image
+    }
+    
+    func placePiece(btnTag: Int) -> Int {
+        // 1=>cross, 2=>nought
+        var piece = isCross ? 1 : 2
+        boardStatus[btnTag] = piece
+        pieceCount++
+        isCross = !isCross // switch player
+        return self.whoWin()
+    }
+    
+    func isTileEmpty(btnTag: Int) -> Bool {
+        return boardStatus[btnTag] == 0
+    }
+    
+    
+    // -1=>continue, 0=>draw, 1=>cross, 2=>nought
+    private func whoWin() -> Int {
+        if pieceCount < 5 {
+            return -1;
+        }
+        
+        for status in winningStatuses {
+            if boardStatus[status[0]] != 0
+               && boardStatus[status[0]] == boardStatus[status[1]]
+               && boardStatus[status[0]] == boardStatus[status[2]] {
+                    return boardStatus[status[0]]
+            }
+        }
+        if pieceCount >= 9 {
+            return 0
+        } else {
+            return -1
+        }
     }
 }
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,13 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let gc = GameController()
+    var gc = GameController()
+    var whoWin = -1
+    
     @IBAction func placeSymbol(sender: AnyObject) {
-        var image = gc.placeSymbol()
+        if whoWin > -1 || !gc.isTileEmpty(sender.tag){
+            return
+        }
+        var image = gc.getSymbol()
         sender.setImage(image, forState: .Normal)
+        whoWin = gc.placePiece(sender.tag);
+        
+        switch whoWin {
+        case 0:
+            resultLabel.text = "It's a draw"
+            showPlayAgainBtn()
+        case 1, 2:
+            resultLabel.text = "Player \(whoWin) Won!"
+            showPlayAgainBtn()
+        default:
+            resultLabel.text = ""
+        }
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var playAgainBtn: UIButton!
+    
+    
+    @IBAction func playAgainBtn(sender: AnyObject) {
+        var button: UIButton
+        for subview in view.subviews {
+            if subview.isKindOfClass(UIButton) {
+                subview.setImage(nil, forState: .Normal)
+            }
+        }
+        whoWin = -1
+        gc = GameController()
+        hidePlayAgainBtn()
+        resultLabel.text = ""
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        hidePlayAgainBtn()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,7 +59,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+    
+    func showPlayAgainBtn() {
+        playAgainBtn.hidden = false
+        UIView.animateWithDuration(0.5, animations: {() -> Void in
+            self.playAgainBtn.center = CGPointMake(self.playAgainBtn.center.x + 600, self.playAgainBtn.center.y)
+        })
+    }
+    
+    func hidePlayAgainBtn() {
+        playAgainBtn.hidden = true
+        playAgainBtn.center = CGPointMake(playAgainBtn.center.x - 600, playAgainBtn.center.y)
+    }
 
 }
 
